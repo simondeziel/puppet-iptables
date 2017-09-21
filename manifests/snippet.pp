04d7c1ae2bc4407141bd1f5fb166740cc1126d72
Simon Deziel <simon@sdeziel.info>
2017-09-21 13:47:45 -0400

    firewall::snippet: remove the order param and embed the number as a mandatory prefix in the name

--- manifests/snippet.pp
+++ manifests/snippet.pp
@@ -4,7 +4,6 @@
 # Deploy commented ruleset snippets in /etc/iptables/snippet/${name}.v[46]
 #
 define firewall::snippet (
-  Variant[Integer[10,99],String[2]] $order,
   Optional[String] $iptables_content       = undef,
   Optional[String] $iptables_source        = undef,
   Optional[String] $ip6tables_content      = undef,
@@ -12,6 +11,10 @@ define firewall::snippet (
   String $group                            = $firewall::group,
   String $mode                             = $firewall::mode,
 ) {
+  if $name !~ /^\d\d/ {
+    fail("${module_name} the name of the snippet must begin with at least 2 digits, like 01-input.")
+  }
+
   # restrict access to rulesets
   File {
     owner => 0,
@@ -19,12 +22,12 @@ define firewall::snippet (
     mode  => $mode,
   }
 
-  file { "${firewall::snippet_dir}/${order}-${name}.v4":
+  file { "${firewall::snippet_dir}/${name}.v4":
     content => $iptables_content,
     source  => $iptables_source,
     notify  => Exec['iptables-aggregate'],
   }
-  file { "${firewall::snippet_dir}/${order}-${name}.v6":
+  file { "${firewall::snippet_dir}/${name}.v6":
     content => $ip6tables_content,
     source  => $ip6tables_source,
     notify  => Exec['ip6tables-aggregate'],
