7ecbbdbd833671f2722018bbbf299602435e7e25
Simon Deziel <simon@sdeziel.info>
2017-09-20 21:10:38 -0400

    firewall: test deplyed rulesets

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,18 +5,23 @@
 # and save working ones in /etc/iptables/rules.v[46]
 #
 class firewall (
-  Optional[String] $iptables_content           = undef,
-  Optional[String] $iptables_source            = undef,
-  String $iptables_filename                    = $firewall::params::iptables_filename,
-  String $iptables_in_filename                 = $firewall::params::iptables_in_filename,
-  Optional[String] $ip6tables_content          = undef,
-  Optional[String] $ip6tables_source           = undef,
-  String $ip6tables_filename                   = $firewall::params::ip6tables_filename,
-  String $ip6tables_in_filename                = $firewall::params::ip6tables_in_filename,
-  String $group                                = $firewall::params::group,
-  String $mode                                 = $firewall::params::mode,
-  Stdlib::AbsolutePath $rules_dir              = $firewall::params::rules_dir,
-  Array[String] $package_names                 = $firewall::params::package_names,
+  Optional[String] $iptables_content      = undef,
+  Optional[String] $iptables_source       = undef,
+  String $iptables_filename               = $firewall::params::iptables_filename,
+  String $iptables_in_filename            = $firewall::params::iptables_in_filename,
+  Optional[String] $ip6tables_content     = undef,
+  Optional[String] $ip6tables_source      = undef,
+  String $ip6tables_filename              = $firewall::params::ip6tables_filename,
+  String $ip6tables_in_filename           = $firewall::params::ip6tables_in_filename,
+  String $group                           = $firewall::params::group,
+  String $mode                            = $firewall::params::mode,
+  Stdlib::AbsolutePath $rules_dir         = $firewall::params::rules_dir,
+  Stdlib::AbsolutePath $snippet_dir       = $firewall::params::snippet_dir,
+  Array[String] $package_names            = $firewall::params::package_names,
+  Stdlib::AbsolutePath $iptables_save     = $firewall::params::iptables_save,
+  Stdlib::AbsolutePath $ip6tables_save    = $firewall::params::ip6tables_save,
+  Stdlib::AbsolutePath $iptables_restore  = $firewall::params::iptables_restore,
+  Stdlib::AbsolutePath $ip6tables_restore = $firewall::params::ip6tables_restore,
 ) inherits firewall::params {
 
   ensure_packages($package_names)
@@ -25,14 +30,22 @@ class firewall (
   $ip6tables_file    = "${rules_dir}/${ip6tables_filename}"
   $ip6tables_in_file = "${rules_dir}/${ip6tables_in_filename}"
 
-  file { $iptables_in_file:
-    content => $iptables_content,
-    source  => $iptables_source,
+  file { $snippet_dir:
+    ensure  => directory,
     owner   => 0,
-    group   => $group,
-    mode    => $mode,
+    group   => 0,
+    mode    => '0755',
     require => Package['iptables-persistent'],
-    notify  => Exec['iptables-restore'],
+  }
+  file { $iptables_in_file:
+    content      => $iptables_content,
+    source       => $iptables_source,
+    owner        => 0,
+    group        => $group,
+    mode         => $mode,
+    validate_cmd => "${iptables_restore} --test < %",
+    require      => Package['iptables-persistent'],
+    notify       => Exec['iptables-restore'],
   }
   file { [$iptables_file,$ip6tables_file]:
     owner   => 0,
@@ -42,39 +55,36 @@ class firewall (
   }
 
   exec { 'iptables-restore':
-    command     => "iptables-restore < ${iptables_in_file}",
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    command     => "${iptables_restore} < ${iptables_in_file}",
     refreshonly => true,
     require     => Package['iptables'],
     notify      => Exec['iptables-save'],
   }
   exec { 'iptables-save':
-    command     => "iptables-save > ${iptables_file}",
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    command     => "${iptables_save} > ${iptables_file}",
     refreshonly => true,
     require     => File[$iptables_file],
   }
 
   file { $ip6tables_in_file:
-    content => $ip6tables_content,
-    source  => $ip6tables_source,
-    owner   => 0,
-    group   => $group,
-    mode    => $mode,
-    require => Package['iptables-persistent'],
-    notify  => Exec['ip6tables-restore'],
+    content      => $ip6tables_content,
+    source       => $ip6tables_source,
+    owner        => 0,
+    group        => $group,
+    mode         => $mode,
+    validate_cmd => "${ip6tables_restore} --test < %",
+    require      => Package['iptables-persistent'],
+    notify       => Exec['ip6tables-restore'],
   }
 
   exec { 'ip6tables-restore':
-    command     => "ip6tables-restore < ${ip6tables_in_file}",
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    command     => "${ip6tables_restore} < ${ip6tables_in_file}",
     refreshonly => true,
     require     => Package['iptables'],
     notify      => Exec['ip6tables-save'],
   }
   exec { 'ip6tables-save':
-    command     => "ip6tables-save > ${ip6tables_file}",
-    path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+    command     => "/sbin/ip6tables-save > ${ip6tables_file}",
     refreshonly => true,
     require     => File[$ip6tables_file],
   }
--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,12 @@ class firewall::params {
   $iptables_in_filename  = 'rules.v4.in'
   $ip6tables_filename    = 'rules.v6'
   $ip6tables_in_filename = 'rules.v6.in'
+  $iptables_save         = '/sbin/iptables-save'
+  $ip6tables_save        = '/sbin/ip6tables-save'
+  $iptables_restore      = '/sbin/iptables-restore'
+  $ip6tables_restore     = '/sbin/ip6tables-restore'
   $rules_dir             = '/etc/iptables'
+  $snippet_dir           = '/etc/iptables/snippet'
   $group                 = 'sudo'
   $mode                  = '0640'
   $package_names         = ['iptables','iptables-persistent']
