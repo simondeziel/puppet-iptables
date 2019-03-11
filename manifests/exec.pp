5dc4227c5101b1a69afbf102910f5b652d0b46ca
Simon Deziel <simon@sdeziel.info>
2019-03-11 16:26:25 -0400

    iptables::exec: aggregate rules if the aggregated files are missing OR empty

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -16,14 +16,14 @@ class iptables::exec inherits iptables {
     exec { 'iptables-aggregate':
       command => "/bin/cat ${snippet_dir}/*.v4 > ${iptables_aggregated_file}",
       umask   => '0077',
-      # only if missing aggregated file or if aggregated file is older than snippet dir
-      onlyif  => "[ ! -e \"${iptables_aggregated_file}\" ] || [ \"${iptables_aggregated_file}\" -ot \"${snippet_dir}\" ]",
+      # only if missing/empty aggregated file or if aggregated file is older than snippet dir
+      onlyif  => "[ ! -s \"${iptables_aggregated_file}\" ] || [ \"${iptables_aggregated_file}\" -ot \"${snippet_dir}\" ]",
     }
     exec { 'ip6tables-aggregate':
       command => "/bin/cat ${snippet_dir}/*.v6 > ${ip6tables_aggregated_file}",
       umask   => '0077',
-      # only if missing aggregated file or if aggregated file is older than snippet dir
-      onlyif  => "[ ! -e \"${ip6tables_aggregated_file}\" ] || [ \"${ip6tables_aggregated_file}\" -ot \"${snippet_dir}\" ]",
+      # only if missing/empty aggregated file or if aggregated file is older than snippet dir
+      onlyif  => "[ ! -s \"${ip6tables_aggregated_file}\" ] || [ \"${ip6tables_aggregated_file}\" -ot \"${snippet_dir}\" ]",
     }
 
     # cannot go in iptables::config due to dependency ordering
